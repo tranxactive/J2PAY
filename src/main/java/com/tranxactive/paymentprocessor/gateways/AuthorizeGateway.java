79823fe5ab9a26adff28ac1c6f89261b9de0a549
Tkhan <tkhan@eplanetcom.com>
2018-03-12 20:23:12 +0500

    just to test my commit blank line added

--- src/main/java/com/tranxactive/paymentprocessor/gateways/AuthorizeGateway.java
+++ src/main/java/com/tranxactive/paymentprocessor/gateways/AuthorizeGateway.java
@@ -2,7 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ 
+*/
 package com.tranxactive.paymentprocessor.gateways;
 
 import com.tranxactive.paymentprocessor.gateways.core.Gateway;
